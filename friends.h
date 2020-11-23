660e82d858de061eb53df59d240e78181d48d003
hlouzada <hendrik.louzada2012@gmail.com>
2020-11-23 17:15:26 -0300

    New header file for user and friend prototypes

---
+++ friends.h
@@ -0,0 +1,15 @@
+#ifndef friends_h_
+#define friends_h_
+
+#include "users.h"
+
+
+typedef struct FriendNode {
+    User *user;
+    FriendNode *next;
+} FriendNode;
+
+
+void add_friend(FriendNode *p, User *u);
+
+#endif
---
+++ users.h
@@ -0,0 +1,25 @@
+#ifndef users_h_
+#define users_h_
+
+#include "messages.h"
+
+
+typedef struct User {
+    char* name;
+    char* nick;
+    MessageNode* first_message;
+} User;
+
+
+typedef struct UserNode {
+    User *user;
+    UserNode *next;
+} UserNode;
+
+
+User new_user(char* name, char* nick);
+
+void push(UserNode *p, User *u);
+
+
+#endif
