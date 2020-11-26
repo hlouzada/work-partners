fb4b317e8875f2341aa902b110b587136673a7f2
hlouzada <hendrik.louzada2012@gmail.com>
2020-11-26 18:56:54 -0300

    Finished? friend API

--- friends.c
+++ friends.c
@@ -3,32 +3,76 @@
 #include "friends.h"
 #include "users.h"
 
-Friend * new_friend(User *from, User *to) {
+FriendRequest * new_friendrequest(User *from, User *to) {
+    FriendRequest *newrequest = NULL;
+
+    newrequest = (FriendRequest *) malloc(sizeof(FriendRequest));
+    if (!newrequest) {
+        exit(1);
+    }
+
+    newrequest -> from = from;
+    newrequest -> to = to;
+    newrequest -> isfriend = false;
+
+    return newrequest;
+}
+
+Friend * new_friend(FriendRequest *friendrequest) {
     Friend *newfriend = NULL;
+
     newfriend = (Friend *) malloc(sizeof(Friend));
-    newfriend -> from = from;
-    newfriend -> to = to;
-    newfriend -> isfriend = false;
+    if (!newfriend) {
+        exit(1);
+    }
+
+    newfriend -> friend_request = friendrequest;
+    newfriend -> next = NULL;
 
     return newfriend;
 }
 
-void send_friend_request(User *from, User *to) {
-    Friend *newfriend = new_friend(from, to);
-    
-    FriendNode *new_friendnode_from = NULL;
-    new_friendnode_from = (FriendNode *) malloc(sizeof(FriendNode));
-    new_friendnode_from -> friends = newfriend;
-    new_friendnode_from -> next = *(from -> friend_node);
-    *(from -> friend_node) = new_friendnode_from;
+FriendList * new_friendlist(Friend *newfriend) {
+    FriendList *new_friendlist = NULL;
 
-    FriendNode *new_friendnode_to = NULL;
-    new_friendnode_to = (FriendNode *) malloc(sizeof(FriendNode));
-    new_friendnode_to -> friends = newfriend;
-    new_friendnode_to -> next = *(to -> friend_node);
-    *(to -> friend_node) = new_friendnode_to;
+    new_friendlist = (FriendList *) malloc(sizeof(FriendList));
+    if (!new_friendlist) {
+        exit(1);
+    }
+
+    new_friendlist -> start = newfriend;
+    new_friendlist -> end = newfriend;
+
+    return new_friendlist;
 }
 
-void add_friend(FriendNode *friendnode) {
-    friendnode -> friends -> isfriend = true;
+void add_user_request(User *user, FriendRequest *newrequest) {
+    if (user -> friend_list == NULL) {
+        Friend *newfriend = new_friend(newrequest);
+        FriendList* newfriendlist = new_friendlist(newfriend);
+
+        user -> friend_list = newfriendlist;
+    } else {
+        Friend *newfriend = new_friend(newrequest);
+
+        user -> friend_list -> start -> next = newfriend;
+        user -> friend_list -> end = newfriend;
+    }
+}
+
+
+void add_friend_request(User *from, User *to) {
+    FriendRequest* newrequest = new_friendrequest(from, to);
+
+    add_user_request(from, newrequest);
+
+    add_user_request(to, newrequest);
+}
+
+void add_friend(Friend *friendnode) {
+    friendnode -> friend_request -> isfriend = true;
+}
+
+bool is_friend(Friend *friendnode) {
+    return friendnode -> friend_request -> isfriend;
 }
